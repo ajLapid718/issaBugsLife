3cb160daf9fdeb91fa9b01a6bd9417be5761b786
Allan James Lapid <ajLapid718@gmail.com>
2019-01-12 21:50:24 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Request from './Request.js';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      requestedZip: ""
+    }
+  }
+
+  updateZip = (event) => {
+      this.setState({
+        requestedZip: event.target.value
+    })
+    // alert("event.target.value: " + String(event.target.value) + ", requestedZip: " + String(this.state.requestedZip))
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <div className="navigation">
+          <div className="Title">Zip City Search</div>
+          <input type="number" onChange={this.updateZip} defaultValue="11234" />
+          <div className="empty"> Zip City Search </div>
+        </div>
+        <div className="allResults">
+          <Request requestZip={this.state.requestedZip} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
